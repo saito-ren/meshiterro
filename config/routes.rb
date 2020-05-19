9b7127e913c24785e74b969af95395988ee6950c
vagrant <vagrant@localhost.localdomain>
2020-05-19 10:44:43 +0000

    [Finish]5-5

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'post_images#index'
@@ -7,4 +8,5 @@ Rails.application.routes.draw do
   	resource :favorites, only: [:create, :destroy]
   	resource :post_comments, only: [:create, :destroy]
   end
+  resources :users, only: [:show]
 end
