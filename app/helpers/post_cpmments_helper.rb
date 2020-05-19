30e494fb0c466f74d1667549d889938473297878
vagrant <vagrant@localhost.localdomain>
2020-05-19 07:01:21 +0000

    [Finish]5-4

---
+++ app/helpers/post_cpmments_helper.rb
@@ -0,0 +1,2 @@
+module PostCpmmentsHelper
+end
---
+++ test/controllers/post_cpmments_controller_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class PostCpmmentsControllerTest < ActionDispatch::IntegrationTest
+  # test "the truth" do
+  #   assert true
+  # end
+end
